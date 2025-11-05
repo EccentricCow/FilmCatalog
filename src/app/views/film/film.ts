f35c854caa5adedd1e991c517b5e50002f4d04a4
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-05 19:06:32 +0700

    feat: create app and configure initial structure

---
+++ src/app/views/film/film.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film',
+  imports: [],
+  templateUrl: './film.html',
+  styleUrl: './film.scss',
+})
+export class Film {
+
+}

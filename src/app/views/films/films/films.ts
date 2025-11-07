d53714fe5d79012048a23373b6c7a2e97b636b02
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-07 13:56:07 +0700

    feat: add films list

--- src/app/views/films/films/films.ts
+++ src/app/views/films/films/films.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
+import { FilmsService } from '../../../services/films.service';
+import {
+  FilmResponseType,
+  FilmsResponseType, GenresResponseType,
+} from '../../../../types/responses/films-response.type';
+import { FilmCard } from '../../../components/film-card/film-card';
+import {finalize} from 'rxjs';
+import {Loader} from '../../../components/loader/loader';
+import {FilmType, Genre} from '../../../../types/film.type';
 
 @Component({
   selector: 'app-films',
-  imports: [],
+  imports: [FilmCard, Loader],
   templateUrl: './films.html',
   styleUrl: './films.scss',
 })
-export class Films {
+export class Films implements OnInit {
+  private readonly _filmsService = inject(FilmsService);
 
+  protected _isLoading = signal<boolean>(false);
+  private _films = signal<FilmResponseType[]>([]);
+  protected _genres = signal<Genre[]>([]);
+  protected readonly _filmsWithGenres = computed((): FilmType[] =>
+    this._films().map(film => ({
+      ...film,
+      genres: film.genre_ids.map(id => (this._genres() ?? []).find(g => g.id === id)?.name ?? '')
+    }))
+  );
+
+  constructor() {  }
+
+  public ngOnInit(): void {
+    this._isLoading.set(true);
+    this._filmsService.getFilms()
+      .pipe(finalize((): void => this._isLoading.set(false)))
+      .subscribe({
+        next: (filmsResponse: FilmsResponseType): void => {
+          if (filmsResponse) {
+            this._films.set(filmsResponse.results);
+          }
+        },
+        error: (err): void => {
+          console.error('Error loading films', err);
+        },
+      });
+
+    this._filmsService.getGenres()
+      .subscribe({
+        next: (genresResponse: GenresResponseType): void => {
+          if (genresResponse?.genres.length > 0) {
+            this._genres.set(genresResponse.genres);
+          }
+        },
+        error: (err): void => {
+          console.error('Error loading genres', err);
+        },
+      });
+  }
 }

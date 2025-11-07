5ce9c68ca852c03addb09b07d7d57b996fd93271
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-07 13:56:17 +0700

    feat: add film card

---
+++ src/app/components/film-card/film-card.ts
@@ -0,0 +1,56 @@
+import {
+  AfterViewInit,
+  Component,
+  computed, ElementRef,
+  inject,
+  input, PLATFORM_ID, signal,
+} from '@angular/core';
+import { FilmResponseType } from '../../../types/responses/films-response.type';
+import { environment } from '../../../environments/environment';
+import {FilmPopoverService} from '../../services/film-popover.service';
+import {FilmPopover} from '../film-popover/film-popover';
+import {isPlatformServer} from '@angular/common';
+
+@Component({
+  selector: 'film-card',
+  imports: [
+    FilmPopover
+  ],
+  templateUrl: './film-card.html',
+  styleUrl: './film-card.scss',
+})
+export class FilmCard implements AfterViewInit {
+  public film = input<FilmResponseType>();
+
+  protected readonly _posterPath = computed((): string =>
+    this.film()?.poster_path
+      ? environment.tmdbApiPosterBaseUrl + this.film()?.poster_path
+      : '/no-movie.png'
+  );
+
+  // popover logic
+  protected readonly _filmPopoverService = inject(FilmPopoverService);
+  constructor(private el: ElementRef) {  }
+
+  popoverPosition = signal<'left' | 'right'>('right');
+  protected _isPopoverVisible = computed<boolean>((): boolean => {
+    return this._filmPopoverService.activeFilm() === this.film()?.id;
+  });
+
+  private readonly _platformId = inject(PLATFORM_ID);
+  public ngAfterViewInit(): void {
+    if (!isPlatformServer(this._platformId)) {
+      const cardRect = (this.el.nativeElement as HTMLDivElement).getBoundingClientRect();
+      const screenWidth = window.innerWidth;
+      cardRect.right + cardRect.width + 10 > screenWidth ? this.popoverPosition.set('left') : this.popoverPosition.set('right');
+    }
+  }
+
+  protected _onMouseEnter(): void {
+    this._filmPopoverService.setHoveredFilm(this.film()?.id ?? null)
+  }
+
+  protected _onMouseLeave(): void {
+    this._filmPopoverService.setHoveredFilm(null)
+  }
+}

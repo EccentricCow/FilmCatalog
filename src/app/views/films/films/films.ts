6f7820798c490e8fef21dbdab471e666840ad9cb
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-07 16:28:35 +0700

    feat: films list with pagination

--- src/app/views/films/films/films.ts
+++ src/app/views/films/films/films.ts
@@ -1,17 +1,17 @@
 import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import { FilmsService } from '../../../services/films.service';
 import {
-  FilmResponseType,
   FilmsResponseType, GenresResponseType,
 } from '../../../../types/responses/films-response.type';
 import { FilmCard } from '../../../components/film-card/film-card';
 import {finalize} from 'rxjs';
 import {Loader} from '../../../components/loader/loader';
 import {FilmType, Genre} from '../../../../types/film.type';
+import {PaginationComponent} from '../../../components/pagination/pagination';
 
 @Component({
   selector: 'app-films',
-  imports: [FilmCard, Loader],
+  imports: [FilmCard, Loader, PaginationComponent],
   templateUrl: './films.html',
   styleUrl: './films.scss',
 })
@@ -19,31 +19,18 @@ export class Films implements OnInit {
   private readonly _filmsService = inject(FilmsService);
 
   protected _isLoading = signal<boolean>(false);
-  private _films = signal<FilmResponseType[]>([]);
+  protected _films = signal<FilmsResponseType>({} as FilmsResponseType);
+  protected _filmsPages = computed<{total: number, current: number}>((): {total: number, current: number} => ({total: this._films().total_pages, current: this._films().page}));
   protected _genres = signal<Genre[]>([]);
   protected readonly _filmsWithGenres = computed((): FilmType[] =>
-    this._films().map(film => ({
+    this._films().results.map(film => ({
       ...film,
       genres: film.genre_ids.map(id => (this._genres() ?? []).find(g => g.id === id)?.name ?? '')
     }))
   );
 
-  constructor() {  }
-
   public ngOnInit(): void {
-    this._isLoading.set(true);
-    this._filmsService.getFilms()
-      .pipe(finalize((): void => this._isLoading.set(false)))
-      .subscribe({
-        next: (filmsResponse: FilmsResponseType): void => {
-          if (filmsResponse) {
-            this._films.set(filmsResponse.results);
-          }
-        },
-        error: (err): void => {
-          console.error('Error loading films', err);
-        },
-      });
+    this._fetchPage();
 
     this._filmsService.getGenres()
       .subscribe({
@@ -54,6 +41,26 @@ export class Films implements OnInit {
         },
         error: (err): void => {
           console.error('Error loading genres', err);
+        },
+      });
+  }
+
+  protected _loadPage(page: number): void {
+   this._fetchPage(page);
+  }
+
+  private _fetchPage(page?: number): void {
+    this._isLoading.set(true);
+    this._filmsService.getFilms(page)
+      .pipe(finalize((): void => this._isLoading.set(false)))
+      .subscribe({
+        next: (filmsResponse: FilmsResponseType): void => {
+          if (filmsResponse) {
+            this._films.set({...filmsResponse, total_pages: filmsResponse.total_pages > 100 ?  100 : filmsResponse.total_pages});
+          }
+        },
+        error: (err): void => {
+          console.error('Error loading films', err);
         },
       });
   }

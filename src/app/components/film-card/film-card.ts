3ce244893e346753a219d7e340addb06b19cc116
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-07 22:04:46 +0700

    feat: cards behavior on mobiles

--- src/app/components/film-card/film-card.ts
+++ src/app/components/film-card/film-card.ts
@@ -1,7 +1,7 @@
 import {
   AfterViewInit,
   Component,
-  computed, ElementRef,
+  computed, ElementRef, HostListener,
   inject,
   input, PLATFORM_ID, signal,
 } from '@angular/core';
@@ -29,8 +29,19 @@ export class FilmCard implements AfterViewInit {
   );
 
   // popover logic
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    if (!this.isDesktop() && this._isPopoverVisible()) {
+      const clickedInside = this.el.nativeElement.contains(event.target);
+      if (!clickedInside) {
+        this._filmPopoverService.setHoveredFilm(null);
+      }
+    }
+  }
+
   protected readonly _filmPopoverService = inject(FilmPopoverService);
   constructor(private el: ElementRef) {  }
+  protected readonly isDesktop = signal(true);
 
   popoverPosition = signal<'left' | 'right'>('right');
   protected _isPopoverVisible = computed<boolean>((): boolean => {
@@ -40,17 +51,39 @@ export class FilmCard implements AfterViewInit {
   private readonly _platformId = inject(PLATFORM_ID);
   public ngAfterViewInit(): void {
     if (!isPlatformServer(this._platformId)) {
-      const cardRect = (this.el.nativeElement as HTMLDivElement).getBoundingClientRect();
-      const screenWidth = window.innerWidth;
-      cardRect.right + cardRect.width + 10 > screenWidth ? this.popoverPosition.set('left') : this.popoverPosition.set('right');
+      const mediaQuery = window.matchMedia('(min-width: 769px)');
+      this.isDesktop.set(mediaQuery.matches);
+
+      mediaQuery.addEventListener('change', e => {
+        this.isDesktop.set(e.matches);
+      });
+
+      if (this.isDesktop()) {
+        const cardRect = (this.el.nativeElement as HTMLDivElement).getBoundingClientRect();
+        const screenWidth = window.innerWidth;
+        cardRect.right + cardRect.width + 10 > screenWidth
+          ? this.popoverPosition.set('left')
+          : this.popoverPosition.set('right');
+      }
     }
   }
 
   protected _onMouseEnter(): void {
-    this._filmPopoverService.setHoveredFilm(this.film().id)
+    if (this.isDesktop()) {
+      this._filmPopoverService.setHoveredFilm(this.film().id);
+    }
   }
 
   protected _onMouseLeave(): void {
-    this._filmPopoverService.setHoveredFilm(null)
+    if (this.isDesktop()) {
+      this._filmPopoverService.setHoveredFilm(null);
+    }
+  }
+
+  protected _onClick(): void {
+    if (!this.isDesktop()) {
+      const active = this._filmPopoverService.activeFilm() === this.film().id;
+      this._filmPopoverService.setHoveredFilm(active ? null : this.film().id);
+    }
   }
 }

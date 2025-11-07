613e449929477fe81c1acd4fda3f06553935fe8a
Alexei Derbin <derbinalexei1@gmail.com>
2025-11-07 16:26:33 +0700

    feat: add pagination

---
+++ src/app/components/pagination/pagination.ts
@@ -0,0 +1,26 @@
+import {Component, computed, input, output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {getPaginationArray} from '../../shared/utils/pagination-array.util';
+
+@Component({
+  selector: 'pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.html',
+  styleUrl: './pagination.scss',
+})
+export class PaginationComponent {
+  public allPagesCount = input.required<number>();
+  public currentPage = input.required<number>();
+
+  protected readonly _math = Math;
+
+  protected _paginationArray = computed<{nums: number[], leftBack: boolean, rightForward: boolean}>((): {nums: number[], leftBack: boolean, rightForward: boolean} => getPaginationArray(this.currentPage(), this.allPagesCount()));
+
+  pageChange = output<number>();
+
+  onClick(page: number): void {
+    if (page < 1 || page > this.allPagesCount()! || page === this.currentPage()) return;
+    this.pageChange.emit(page);
+  }
+}
